a836359706561f1667c2f1f9f0945b85942cd2cb
Dhanisha <Dhanisha@MacBook-Pro.local>
2018-06-26 16:32:15 -0700

    my correction to port

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const app = express(); // generate new application, inside single project we mig
 //  going to use only one app , set up configuration that listen to incoming request
 
 //creating a route handler
+//second argument is the arrow function
 app.get("/", (req, res) => {
 	res.send({ hi: "there" });
 });
 
-app.listen(5000);
+// when ever heroku run our app , it has the ability to injectenviroment variable
+//enviroment variable are the variable they are set in the under lying runtime that node is running on top of.
+//we cant look on heroku hat will be our port ahead of time we need to wait until the very last second
+//until our app is starting to be executed by heroku what the port is.
+//look at the underlying envinroment variable and see if they have set the port for us
+//developemnt enviroment 5000 and production enviroment process.env.port
+const PORT = process.env.PORT || 5000;
+// this line will instruct express to tell m=node to listen on port 5000
+app.listen(PORT); //localhost:5000
